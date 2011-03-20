c0e1e76a027bfde9bdff275e7a4fdf1fc5f12209
T S <YenTheFirst@gmail.com>
2011-03-20 12:57:58 -0700

    initial rails create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hubbuds::Application.config.secret_token = '74a8a08bdafd5a1f9d6f0dd3715275a9e81c0af577c33f0482c0eab2404d40939d6c2caf4b40d7948d85f1d3c43f76341ed64cc1f4a1b4c5521e5a5d312d1f3a'
